8d0883bd0875707da35f85b4b30b05d5d96a7cbe
Will Green <will@hotgazpacho.org>
2011-04-17 11:52:16 -0400

    Add a server implementation

---
+++ zmqPubSub.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace zmqPubSub.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new ZmqPubSubServer("tcp://0.0.0.0:12345", "tcp://0.0.0.0:54321", Encoding.Unicode);
+            server.Start();
+        }
+    }
+}
---
+++ zmqPubSub.Server/ZmqPubSubServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ZMQ;
+using System.Text;
+
+namespace zmqPubSub.Server
+{
+    public class ZmqPubSubServer
+    {
+        public string PublishAddress { get; private set; }
+        public string ListenAddress { get; private set; }
+        public Encoding MessageEncoding { get; private set; }
+
+        public ZmqPubSubServer(string publishAddress, string listenAddress, Encoding messageEncoding)
+        {
+            PublishAddress = publishAddress;
+            ListenAddress = listenAddress;
+            MessageEncoding = messageEncoding;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting Zmq Pub/Sub Server...");
+            using (var context = new Context(1))
+            {
+                var receiver = new ZmqMessageReceiver(context, ListenAddress, MessageEncoding);
+                var sender = new ZmqMessageSender(context, PublishAddress, MessageEncoding);
+                var messageBus = new MessageBus(receiver, sender);
+                Console.WriteLine(string.Format("- Messages will be encoded as {0}", MessageEncoding.EncodingName));
+                Console.WriteLine(string.Format("-- Listening for messages on {0}", ListenAddress));
+                Console.WriteLine(string.Format("-- Publishing messages on {0}", PublishAddress));
+                messageBus.Start();
+            }
+            Console.WriteLine("Zmq Pub/Sub Server Shutting down.");
+        }
+    }
+}
